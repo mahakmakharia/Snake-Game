3a3cd364baa83b4645d3ca065b881dc88d254c3f
mahakmakharia <makhariamahak@gmail.com>
2020-04-29 15:56:28 +0530

    added images

---
+++ snake.js
@@ -0,0 +1,28 @@
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+
+const box = 32;
+
+
+
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
